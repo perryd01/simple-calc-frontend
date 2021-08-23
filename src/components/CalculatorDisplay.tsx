6af56528adfeca77ef529cfed54f81decfacb3d6
Dániel Miklós <daniel.miklos0@gmail.com>
2021-08-23 13:20:33 +0200

    trying out zustand checkpoint

--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -1,16 +1,13 @@
 import { useState } from "react"
 
-export function CalculatorDisplay(props: { value: number }) {
-  const [upper, setUpper] = useState(0)
-  const [lower, setLower] = useState(0)
-
+export function CalculatorDisplay(props: { upper: number; lower: string }) {
   return (
     <div className="w-11/12 py-4 bg-gray-50 border-4 mx-auto pr-8 text-right">
-      <div id="upper">
-        {upper === lower ? <p className="invisible">{upper}</p> : <p>{upper}</p>}
+      <div id="upper" className="text-xl">
+        <p>{props.upper}</p>
       </div>
-      <div id="lower" className="text-2xl font-bold">
-        <p>{lower}</p>
+      <div id="lower" className="text-2xl text-gray-500">
+        <p>{props.lower}</p>
       </div>
     </div>
   )
--- src/components/CalculatorKeypad.tsx
+++ src/components/CalculatorKeypad.tsx
@@ -1,22 +1,27 @@
 import { CalculatorButton } from "./CalculatorButton"
+import { WritableCalcDisplayState } from "./CalculatorPanel"
 
-export function CalculatorKeyPad() {
+export function CalculatorKeyPad(props: { func: WritableCalcDisplayState }) {
   return (
-    <div className="grid grid-cols-3 gap-4 w-11/12 mx-auto pt-8">
+    <div className="grid grid-cols-4 gap-4 w-11/12 mx-auto pt-8">
       <CalculatorButton value={1} />
       <CalculatorButton value={2} />
       <CalculatorButton value={3} />
+      <CalculatorButton value="+" styleName="bg-yellow-200" />
       <CalculatorButton value={4} />
       <CalculatorButton value={5} />
       <CalculatorButton value={6} />
+      <CalculatorButton value="-" styleName="bg-yellow-200" />
       <CalculatorButton value={7} />
       <CalculatorButton value={8} />
       <CalculatorButton value={9} />
-      <CalculatorButton value={0} styleName="col-span-2" />
+      <CalculatorButton value="x" styleName="bg-yellow-200" />
+      <CalculatorButton value={0} styleName="col-span-3" />
+      <CalculatorButton value="/" styleName="bg-yellow-200" />
       <CalculatorButton value="clear" />
-      <CalculatorButton value="M LOAD" />
-      <CalculatorButton value="M SAVE" />
-      <CalculatorButton value="🤘🏼" />
+      <CalculatorButton value="M LOAD" styleName="italic" />
+      <CalculatorButton value="M SAVE" styleName="italic" />
+      <CalculatorButton value="M RESET" styleName="italic" />
     </div>
   )
 }
